88c68f698c4e074649b66d62cc18abc23dad94c7
PritamBiswas489 <pritam.biswas489@gmail.com>
2023-07-04 11:21:27 +0530

    Update with new code 04-07-2023

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,26 @@
-import '@/styles/globals.css'
+import "bootstrap/dist/css/bootstrap.css";
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
+import '@/assets/fonts/stylesheet.css';
+import '@/assets/css/style.scss';
+
+import "nprogress/nprogress.css";
+import '@/component/TopProgressBar.css';
+import dynamic from "next/dynamic";
+const TopProgressBar = dynamic(
+  () => {
+    return import("component/TopProgressBar");
+  },
+  { ssr: false },
+);
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <>
+  <TopProgressBar />
+   
+    <Component {...pageProps} />
+    
+  </>
+ );
 }
